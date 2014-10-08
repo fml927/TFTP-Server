719860aea4516e12ee9e48bb625f32604135f0f9
seansmccullough <smccullough@xpanxion.com>
2014-10-08 12:12:51 -0500

    lunch break

--- TFTP/Server.cs
+++ TFTP/Server.cs
@@ -10,28 +10,36 @@ namespace TFTP
 {
     class Server
     {
-        private bool running;
-        private UdpClient server;
-        private IPEndPoint endPoint;
+        private bool _running;
+        private IPEndPoint _endPoint;
+        private Semaphore _semaphore;
 
         //need to figure out the port thing.  Do I access connections on all ports?  only port 69?
         public Server(int port)
         {
-            endPoint = new IPEndPoint(IPAddress.Any,port);
-            running = true;
-            server = new UdpClient(endPoint);
+            _endPoint = new IPEndPoint(IPAddress.Any,port);
+            _running = true;
+            //selected 25 arbitrarially
+            _semaphore = new Semaphore(0, 25);
+            loop();
         }
         
         public void loop()
         {
-            while(running)
+            while(_running)
             {
-
+                UdpClient newClient = new UdpClient(_endPoint);
+                //I think this blocks, need to check to make sure
+                byte[] bytes = newClient.Receive(ref _endPoint);
+                Thread t = new Thread(new ParameterizedThreadStart(handleClient));
+                t.Start(new Shared.HandleClientParams { client = newClient, bytes = bytes });
             }
         }
-        public void handleClient()
+        public void handleClient(object o)
         {
-
+            Shared.HandleClientParams param = (Shared.HandleClientParams)o;
+            _semaphore.WaitOne();
+            _semaphore.Release();
         }
     
     }
--- TFTP/Shared.cs
+++ TFTP/Shared.cs
@@ -2,11 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
 
 namespace TFTP
 {
     class Shared
     {
+        public enum OpCode
+        {
+            Read = 1,
+            Write = 2,
+            Data = 3,
+            Acknowledge = 4,
+            Error = 5
+        }
+        public enum ErrorCode
+        {
+            NotDefined = 0,
+            FileNotFound = 1,
+            AccessViolation = 2,
+            DiskFull = 3,
+            IllegalOpeation = 4,
+            UnknownID = 5,
+            FileAlreadyExists = 6,
+            NoSuchUser = 7,
+        }
+        public abstract class Packet
+        {
+            protected byte[] BlockNumber;
+            protected byte[] ModeErrorMessage;
+
+            public abstract byte[] Raw();
+        }
+        public class ReadPacket : Packet
+        {
+            public string Filename { get; private set; }
+            public string Mode { get; private set; }
+            public ReadPacket(byte[] raw)
+            {
+                
+            }
+            public override byte[] Raw()
+            {
+                return new byte[] { 0 };
+            }
+        }
+        public class HandleClientParams
+        {
+            public UdpClient client { get; set; }
+            public byte[] bytes { get; set; }
+        }
     }
 }
