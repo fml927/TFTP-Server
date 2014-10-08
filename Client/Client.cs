b249b5af21d7a4dd2c70a2d320d9b1a06f821abe
seansmccullough <smccullough@xpanxion.com>
2014-10-08 11:02:43 -0500

    more foundation code

--- Client/Client.cs
+++ Client/Client.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace Client
 {
     class Client
     {
+        public Client()
+        {
+
+        }
+
+        public bool Send()
+        {
+            return true;
+        }
+
+        public bool Receive()
+        {
+            return true;
+        }
     }
 }
--- TFTP/Server.cs
+++ TFTP/Server.cs
@@ -2,11 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
 
 namespace TFTP
 {
     class Server
     {
+        private bool running;
+        private UdpClient server;
+        private IPEndPoint endPoint;
+
+        //need to figure out the port thing.  Do I access connections on all ports?  only port 69?
+        public Server(int port)
+        {
+            endPoint = new IPEndPoint(IPAddress.Any,port);
+            running = true;
+            server = new UdpClient(endPoint);
+        }
+        
+        public void loop()
+        {
+            while(running)
+            {
+
+            }
+        }
+        public void handleClient()
+        {
+
+        }
+    
     }
 }
